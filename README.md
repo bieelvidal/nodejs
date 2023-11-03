14f7620f7e3f2e4c4b67565e2eb6ee95ffadf119
bieelvidal <gabrielhelps@hotmail.com>
2023-11-03 13:48:02 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# nodejs
+ Curso node.js
---
+++ nodejs/app.js
@@ -0,0 +1,20 @@
+var  = require("./calculadora");
+
+
+
+console.log(calculadora.mult(10,20));
+console.log(calculadora.soma(20,40))
+calculadora.nome "Calculadora do Gabriel"
+console.log(calculadora.nome);
+
+
+/*var mostrarSite = true;
+const site = "www.guiadoprogramador.com";
+
+console.log("Hello World!");
+console.log("Meu nome é Gabriel");
+console.log ("E eu estou aprendendo Node.js com o Guia do programador");
+
+if(mostrarSite){ // se mostrar site
+    console.log(site); // Printe o site
+}*/
